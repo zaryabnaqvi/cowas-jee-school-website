8db8e4c261c6ad55cf0ffcf1b6eeacb50dcd5012
Zaryab Naqvi <zaryab.110786@gmail.com>
2024-07-06 17:35:50 +0500

    master

--- src/Components/BookAppointment.js
+++ src/Components/BookAppointment.js
@@ -12,7 +12,7 @@ function BookAppointment() {
   const navigate = useNavigate();
 
   const handleBookAppointmentClick = () => {
-    navigate("/appointment");
+    navigate("/accouncements");
   };
 
   return (
