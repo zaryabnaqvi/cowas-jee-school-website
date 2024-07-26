b9f96e74c7c5141270a516395cafd00d824f8fd9
Zaryab Naqvi <103928492+zaryabnaqvi@users.noreply.github.com>
2024-07-26 23:26:06 +0500

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -7,7 +7,7 @@ function NotFound() {
 
   return (
     <div style={{display:'flex',minHeight:"100vh",justifyContent:"center",alignItems:"center"}}>
-      <div style={{fontSize:"6rem",textAlign:"center"}}>Comming Soon</div>
+      <div style={{fontSize:"4rem",textAlign:"center"}}>Comming Soon</div>
      
     </div>
   );
